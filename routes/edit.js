b4db13ee48530bcb3fee2eac86a4dab6fa5908f1
David Lenh <dlenh425@gmail.com>
2023-05-16 13:23:34 -0700

    Added route to get to edit page

--- routes/edit.js
+++ routes/edit.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const editController = require("../controllers/edit");
 
-// router.get("/:id", editController.getEdit);
+router.get("/:id", editController.getEdit);
 router.get("/remove/:id", editController.deleteMed);
-router.post("update/:id", editController.updateMed);
+router.post("/update/:id", editController.updateMed);
 
 module.exports = router;
