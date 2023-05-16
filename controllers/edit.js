983626d228ba5e3d6083751fb2297f10f72242e1
David Lenh <dlenh425@gmail.com>
2023-05-16 13:23:58 -0700

    Added getEdit method to get edit page

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,6 +1,16 @@
 const MedList = require("../models/medlist");
 
 module.exports = {
+    getEdit: async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        try {
+            const meds = await MedList.find();
+            res.render("edit.ejs", {medList: meds, idMed: id});
+        } catch (err) {
+            if (err) return res.status(500).send(err);
+        }
+    },
     deleteMed: async (req, res) => {
         const id = req.params.id;
         try {
