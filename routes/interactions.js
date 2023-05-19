a75cfe015d059b71650cae2ac8c49e11a5f380f2
David Lenh <dlenh425@gmail.com>
2023-05-19 13:20:02 -0700

    Removed interactions route

--- routes/interactions.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const interactionsController = require("../controllers/interactionsController");
-
-router.post("/interactions, interactionsController.checkInteractions");
-
-module.exports = router;
