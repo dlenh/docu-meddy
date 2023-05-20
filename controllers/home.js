9d9e031c4cdfd6a5c8620eb142a294cdf37d9a20
David Lenh <dlenh425@gmail.com>
2023-05-20 10:05:10 -0700

    Updated addMed method

--- controllers/home.js
+++ controllers/home.js
@@ -20,54 +20,122 @@ module.exports = {
         }
     },
     addMed: async (req, res) => {
-    const input = req.body.name;
-    const notes = req.body.notes;
-
-    try {
-      const existingMed = await MedList.findOne({ name: { $regex: new RegExp(`^${input}$`, 'i') } });
-      if (existingMed) {
-        req.flash("error", "This medicine is already on your list. You may edit it below.");
-        req.flash("input", existingMed.name); // Use the existing medicine name from the database
-        req.flash("notes", notes);
+      const input = req.body.name;
+      const notes = req.body.notes;
+  
+      try {
+        const existingMed = await MedList.findOne({ name: { $regex: new RegExp(`^${input}$`, 'i') } });
+        if (existingMed) {
+          req.flash("error", "This medicine is already on your list. You may edit it below.");
+          req.flash("input", existingMed.name); // Use the existing medicine name from the database
+          req.flash("notes", notes);
+          return res.redirect("/");
+        } else if (!input) {
+          req.flash("error", "Please enter a medicine name.");
+          req.flash("input", input);
+          req.flash("notes", notes);
+          return res.redirect("/");
+        }
+  
+        const response = await fetch(
+          `https://rxnav.nlm.nih.gov/REST/Prescribe/rxcui.json?name=${input}`
+        );
+        const data = await response.json();
+        if (
+          !data.idGroup ||
+          !data.idGroup.rxnormId ||
+          data.idGroup.rxnormId.length === 0
+        ) {
+          req.flash("error", "Invalid medicine name. Please ensure correct spelling.");
+          req.flash("input", input);
+          req.flash("notes", notes);
+          return res.redirect("/");
+        }
+  
+        const rxcui = data.idGroup.rxnormId[0];
+        const newMed = new MedList({
+          name: input,
+          notes: notes,
+          rxcui: rxcui,
+        });
+        await newMed.save();
+        console.log(newMed);
+        req.flash("success", "Medicine added successfully!");
         return res.redirect("/");
-      } else if (!input) {
-        req.flash("error", "Please enter a medicine name.");
-        req.flash("input", input);
-        req.flash("notes", notes);
+      } catch (err) {
+        console.error(err);
+        req.flash("error", "An error occurred while adding medicine.");
         return res.redirect("/");
       }
-
-      const response = await fetch(
-        `https://rxnav.nlm.nih.gov/REST/Prescribe/rxcui.json?name=${input}`
-      );
-      const data = await response.json();
-      if (
-        !data.idGroup ||
-        !data.idGroup.rxnormId ||
-        data.idGroup.rxnormId.length === 0
-      ) {
-        req.flash("error", "Invalid medicine name. Please ensure correct spelling.");
-        req.flash("input", input);
-        req.flash("notes", notes);
-        return res.redirect("/");
-      }
-
-      const rxcui = data.idGroup.rxnormId[0];
-      const newMed = new MedList({
-        name: input,
-        notes: notes,
-        rxcui: rxcui,
-      });
-      await newMed.save();
-      console.log(newMed);
-      req.flash("success", "Medicine added successfully!");
-      return res.redirect("/");
-    } catch (err) {
-      console.error(err);
-      req.flash("error", "An error occurred while adding medicine.");
-      return res.redirect("/");
     }
-  }
+  //   addMed: async (req, res) => {
+  //     const input = req.body.name.toLowerCase();
+  //     const notes = req.body.notes;
+  //     const  medlist = await MedList.find();
+
+  //     try {
+  //       const existingMed = await MedList.findOne({ name: { $regex: new RegExp(`^${input}$`, 'i') } });
+  //       if (existingMed) {
+  //         req.flash("error", "This medicine is already on your list. You may edit it below.");
+  //         req.flash("input", existingMed.name); // Use the existing medicine name from the database
+  //         req.flash("notes", notes);
+  //         return res.redirect("/");
+  //       } else if (!input) {
+  //         req.flash("error", "Please enter a medicine name.");
+  //         req.flash("input", input);
+  //         req.flash("notes", notes);
+  //         return res.redirect("/");
+  //       }
+
+  //       const response = await fetch(
+  //         `https://rxnav.nlm.nih.gov/REST/Prescribe/rxcui.json?name=${input}`
+  //       );
+  //       const data = await response.json();
+  //       if (
+  //         !data.idGroup ||
+  //         !data.idGroup.rxnormId ||
+  //         data.idGroup.rxnormId.length === 0
+  //       ) {
+  //         req.flash("error", "Invalid medicine name. Please ensure correct spelling.");
+  //         req.flash("input", input);
+  //         req.flash("notes", notes);
+  //         return res.redirect("/");
+  //       }
+
+  //       const rxcui = data.idGroup.rxnormId[0];
+  //       const newMed = new MedList({
+  //         name: input,
+  //         notes: notes,
+  //         rxcui: rxcui,
+  //       });
+
+  //       // const alternativeNamesResponse = await fetch(
+  //       //   `https://rxnav.nlm.nih.gov/REST/ApproximateTerm?term=${input}&maxEntries=10`
+  //       // );
+  //       // const alternativeNamesData = await alternativeNamesResponse.json();
+  //       // const alternativeNames = alternativeNamesData.approximateGroup?.[0]?.candidate?.map(
+  //       //   candidate => candidate.rxcuiName
+  //       // );
+
+        
+  //       req.flash("success", "Medicine added successfully!");
+  //       await newMed.save();
+  //       console.log(newMed);
+        
+  //       return res.render("index.ejs", {
+  //         medList: [...medlist, newMed],
+  //         input: input,
+  //         notes: notes,
+  //         errorMessages: req.flash("error"),
+  //         // alternativeNames: alternativeNames,
+  //         successMessages: req.flash("success"),
+  //       });
+  //     } catch (err) {
+  //       console.error(err);
+  //       req.flash("error", "An error occurred while adding medicine.");
+  //       return res.redirect("/");
+  //     }
+  // }
     // checkInteractions: async (req, res) => {
     //   console.log("Check Interactions method called");
     //   try {
