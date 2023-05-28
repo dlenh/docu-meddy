97bde9d63c07c75f78920551e4c1464c4ee9e777
David Lenh <dlenh425@gmail.com>
2023-05-28 10:46:12 -0700

    Edited addMed method

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
 router.post("/new", homeController.addMed);
+// router.get("/interactions", homeController.checkInteractions);
 
 module.exports = router;
