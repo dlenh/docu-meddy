b1fddcee119268fc15e371b88efa6723d99deeb3
David Lenh <dlenh425@gmail.com>
2023-06-14 12:54:17 -0700

    Set up logging with morgan

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ connectDB();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(logger("dev"));
 
 app.use(
     session({
