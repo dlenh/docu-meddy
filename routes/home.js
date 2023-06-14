271c8c2607801e8e1035823688102e5d5f3e591a
David Lenh <dlenh425@gmail.com>
2023-06-14 12:57:06 -0700

    Added methods from auth controller

--- routes/home.js
+++ routes/home.js
@@ -5,5 +5,10 @@ const homeController = require("../controllers/home");
 router.get("/", homeController.getIndex);
 router.post("/new", homeController.addMed);
 // router.get("/interactions", homeController.checkInteractions);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+rotuer.post("/signup", authController.postSignup);
 
 module.exports = router;
