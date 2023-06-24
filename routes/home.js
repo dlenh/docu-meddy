459bce1c548c9fe7175cb92638ac470c706fa07b
David Lenh <dlenh425@gmail.com>
2023-06-24 08:23:26 -0700

    Routed index to home

--- routes/home.js
+++ routes/home.js
@@ -5,12 +5,13 @@ const authController = require("../controllers/auth");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, homeController.getProfile);
 router.post("/new", homeController.addMed);
 // router.get("/interactions", homeController.checkInteractions);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
-rotuer.post("/signup", authController.postSignup);
+router.post("/signup", authController.postSignup);
 
 module.exports = router;
