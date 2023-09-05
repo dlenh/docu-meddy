d137b025f22c9fd53cf0a0a133bd67f730a7307c
David Lenh <dlenh425@gmail.com>
2023-09-05 15:48:45 -0700

    Commented out interactions route

--- routes/interaction.js
+++ routes/interaction.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const interactionController = require("../controllers/interaction");
+// const express = require("express");
+// const router = express.Router();
+// const interactionController = require("../controllers/interaction");
 
-// router.get("/", homeController.getIndex);
-// router.post("/new", homeController.addMed);
-router.get("/interaction", interactionController.checkInteraction);
+// // router.get("/", homeController.getIndex);
+// // router.post("/new", homeController.addMed);
+// router.get("/interaction", interactionController.checkInteraction);
 
-module.exports = router;
+// module.exports = router;
